ffa95ddcb966df6d9408a9eedbd87ae68b975c82
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-17 12:59:40 -0300

    Feign Clients

--- hr-payroll/src/main/java/br/com/maurigvs/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/com/maurigvs/hrpayroll/HrPayrollApplication.java
@@ -2,12 +2,13 @@ package br.com.maurigvs.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class HrPayrollApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HrPayrollApplication.class, args);
     }
-
 }
--- hr-payroll/src/main/java/br/com/maurigvs/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/maurigvs/hrpayroll/services/PaymentService.java
@@ -2,32 +2,19 @@ package br.com.maurigvs.hrpayroll.services;
 
 import br.com.maurigvs.hrpayroll.entities.Payment;
 import br.com.maurigvs.hrpayroll.entities.Worker;
+import br.com.maurigvs.hrpayroll.feignclients.WorkerFeignClients;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClients workerFeignClients;
 
     public Payment getPayment(Long workerId, int daysWorked){
 
-        Map<String, String> params = new HashMap<>();
-        params.put("id",""+workerId);
-
-        String url = workerHost + "/workers/{id}";
-
-        Worker worker = restTemplate.getForObject(url, Worker.class, params);
-
+        Worker worker = workerFeignClients.selectById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), daysWorked);
     }
 }
