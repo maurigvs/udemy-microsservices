f7f6a889f4170a2e6a42250666e6cb0717eeb221
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-17 12:32:36 -0300

    hr-payroll rest template

--- hr-payroll/src/main/java/br/com/maurigvs/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/maurigvs/hrpayroll/services/PaymentService.java
@@ -1,12 +1,33 @@
 package br.com.maurigvs.hrpayroll.services;
 
 import br.com.maurigvs.hrpayroll.entities.Payment;
+import br.com.maurigvs.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
 
+    @Value("${hr-worker.host}")
+    private String workerHost;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     public Payment getPayment(Long workerId, int daysWorked){
-        return new Payment("Bob Brown", 250.00, daysWorked);
+
+        Map<String, String> params = new HashMap<>();
+        params.put("id",""+workerId);
+
+        String url = workerHost + "/workers/{id}";
+
+        Worker worker = restTemplate.getForObject(url, Worker.class, params);
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), daysWorked);
     }
 }
