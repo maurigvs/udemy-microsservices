79a23b5536e0d5306ea615150f760854596d48b0
Mauri Gonçalves <maurigvs@icloud.com>
2022-08-18 14:57:54 -0300

    hr-eureka-server configuration

--- hr-eureka-server/src/main/java/br/com/maurigvs/hreurekaserver/HrEurekaServerApplication.java
+++ hr-eureka-server/src/main/java/br/com/maurigvs/hreurekaserver/HrEurekaServerApplication.java
@@ -4,12 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
 @SpringBootApplication
+@EnableEurekaServer
 public class HrEurekaServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HrEurekaServerApplication.class, args);
     }
-
 }
